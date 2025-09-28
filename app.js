8bf2f133ddde8c86b9bad8310515e7e5351f8d70
Dang Phuc Long <dangphuclong2019@gmail.com>
2025-09-28 14:11:01 +0700

    Initial commit - prepare for Railway deployment

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// Serve static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirect root to home
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+// Routes cho Black Box Tools
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+// Route cho Black Box - serve trực tiếp black-box.html
+app.get('/black_box', (req, res) => {
+    // Kiểm tra có mã function không (validation sẽ làm ở client-side)
+    const functionCode = req.query.code;
+    if (functionCode) {
+        res.sendFile(path.join(__dirname, 'views', 'black-box.html'));
+    } else {
+        res.redirect('/home');
+    }
+});
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
